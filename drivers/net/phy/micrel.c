3430c2d8a2bc5be1cf46e195d9726a79388f0387
Frodo Lai <frodo_lai@bcmcom.com>
2014-01-13 17:57:14 -0800

    AR6MXS/Q: adding ehternet support.

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -72,6 +72,19 @@ static int kszphy_config_intr(struct phy_device *phydev)
 	return rc < 0 ? rc : 0;
 }
 
+static int ksz9031_config_intr(struct phy_device *phydev)
+{
+	int temp, rc;
+
+	/* set the interrupt pin active low */
+	temp = phy_read(phydev, MII_KSZPHY_CTRL);
+	temp &= ~KSZ9021_CTRL_INT_ACTIVE_HIGH;
+	phy_write(phydev, MII_KSZPHY_CTRL, temp);
+	rc = kszphy_set_interrupt(phydev);
+
+	return rc < 0 ? rc : 0;
+}
+
 static int ksz9021_config_intr(struct phy_device *phydev)
 {
 	int temp, rc;
@@ -187,6 +200,21 @@ static struct phy_driver ksz9021_driver = {
 	.driver		= { .owner = THIS_MODULE, },
 };
 
+static struct phy_driver ksz9031_driver = {
+	.phy_id		= PHY_ID_KSZ9031,
+	.phy_id_mask	= 0x00ffffff,
+	.name		= "Micrel KSZ9031 Gigabit PHY",
+	.features	= (PHY_GBIT_FEATURES | SUPPORTED_Pause
+				| SUPPORTED_Asym_Pause),
+	.flags		= PHY_HAS_MAGICANEG | PHY_HAS_INTERRUPT,
+	.config_init	= kszphy_config_init,
+	.config_aneg	= genphy_config_aneg,
+	.read_status	= genphy_read_status,
+	.ack_interrupt	= kszphy_ack_interrupt,
+	.config_intr	= ksz9031_config_intr,
+	.driver		= { .owner = THIS_MODULE, },
+};
+
 static int __init ksphy_init(void)
 {
 	int ret;
@@ -209,8 +237,14 @@ static int __init ksphy_init(void)
 	if (ret)
 		goto err5;
 
+	ret = phy_driver_register(&ksz9031_driver);
+	if (ret)
+		goto err6;
+
 	return 0;
 
+err6:
+	phy_driver_unregister(&ksz9031_driver);
 err5:
 	phy_driver_unregister(&ks8041_driver);
 err4:
@@ -230,6 +264,7 @@ static void __exit ksphy_exit(void)
 	phy_driver_unregister(&ksz9021_driver);
 	phy_driver_unregister(&ks8041_driver);
 	phy_driver_unregister(&ks8051_driver);
+	phy_driver_unregister(&ksz9031_driver);
 }
 
 module_init(ksphy_init);
@@ -245,6 +280,7 @@ static struct mdio_device_id __maybe_unused micrel_tbl[] = {
 	{ PHY_ID_KS8737, 0x00ffffff },
 	{ PHY_ID_KS8041, 0x00ffffff },
 	{ PHY_ID_KS8051, 0x00ffffff },
+	{ PHY_ID_KSZ9031, 0x00ffffff },
 	{ }
 };
 
